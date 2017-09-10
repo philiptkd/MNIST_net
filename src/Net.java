2ddbca64a0401ddbef232c40ea7f3e569b779316
philiptkd <philiptkd1@gmail.com>
2017-09-09 23:42:35 -0500

    initial commit

---
+++ src/Net.java
@@ -0,0 +1,23 @@
+/*
+ * TODO: create and train a neural net to classify handwritten digits from the MNIST data set
+ * 
+ * figure out how to get the data
+ * 		all at once? one piece at a time?
+ * learn to access the data
+ * create the Net class
+ * 		constructor parameters?
+ * figure out how to find the gradient of a function
+ * learn how to randomly select arrayList elements for stochastic gradient descent
+ * write method for getting net's output with given input and weights/biases
+ * write sigmoid neuron class
+ * at completion of training, write final weights/biases to a file
+ * test on testing set
+ */
+public class Net {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+	}
+
+}
