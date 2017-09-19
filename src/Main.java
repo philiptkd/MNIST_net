9b5311fda08889138b927dd3d268de9cf577aa72
philiptkd <philiptkd1@gmail.com>
2017-09-18 22:27:12 -0500

    prohibitively slow

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
  * 
  * choose eps based on ulp?
  * 		eps = x0*sqrt(ulp)		(if x0 != 0)
- * create function to evaluate cost function at given weights/bias
+
  * restructure net to have weights/biases in one array
  * 
  * figure out how to find the gradient of a function
@@ -13,51 +13,32 @@
  * 			where a is the vector of outputs when x is input, y(x) is the desired output, and n is the numbe4 of training inputs
  * 		let V denote the current vector of weights and biases for the net
  * 		V' = V - eta*grad(C(V))
- * learn how to randomly select arrayList elements for stochastic gradient descent
- * at completion of training, test on testing set
  */
 
 import java.io.IOException;
 import java.util.Random;
-import java.lang.Double;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException{
 		
-//		Net myNet = new Net(28*28, 15, 10);
-//		trainNet(myNet, 30, 10, 3.0);
+		Net myNet = new Net(28*28, 15, 10);
+		trainNet(myNet, 30, 10, 3.0);
+		System.out.println(myNet.getErrorRate());
 	}
 	
-	private static void trainNet(Net myNet, int epochs, int miniBatchSize, double learningRate) throws IOException{
-		double EPSILON = Math.sqrt(Math.pow(2, -53));	//to be used to calculate step size in differentiation
-		
+	private static void trainNet(Net myNet, int epochs, int miniBatchSize, double learningRate) {		
 		//create a list that we can shuffle in order to randomize our mini-batches
 		int[] shuffledList = new int[60000];
 		for(int i=0; i<shuffledList.length; i++) {
 			shuffledList[i] = i;
 		}
-		
-		int numWeights = myNet.inputLayer.length*myNet.hiddenLayer.length + myNet.hiddenLayer.length*myNet.outputLayer.length;
-		int numBiases = myNet.hiddenLayer.length + myNet.outputLayer.length;
-		
+			
 		for(int epoch=0; epoch<epochs; epoch++) {	//for each epoch
 			shuffle(shuffledList);		//shuffle the training set order
 			for(int miniBatch=0; miniBatch<shuffledList.length/miniBatchSize; miniBatch++) {	//for each miniBatch
-				int[] gradientVectorSum = new int[numWeights + numBiases];	//create zero vector for weights/biases
-				//calculate cost function for current weights/biases
-				for(int input=miniBatch*miniBatchSize; input<(miniBatch+1)*miniBatchSize; input++) {	//for each training input
-					//compute partial derivatives dC/dw and dC/db for all weights w and biases b
-					//add this input's gradient vector to gradient vector sum
-				}
-				//divide gradient vector sum by miniBatchSize to get average
-				//update point in weight/bias space
+				myNet.SGD(miniBatch, miniBatchSize, shuffledList, learningRate);	//update weights/biases via stochastic gradient descent
 			}			
 		}
-			
-		//save final weights and biases for testing
-		
-		//check output of net for each test image against its label
-		//compute error rate
 	}
 	
 	//shuffles a list
--- src/Net.java
+++ src/Net.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.channels.FileChannel.MapMode;
@@ -10,14 +11,25 @@ public class Net {
 	byte[] inputLayer;
 	Neuron[] hiddenLayer;
 	Neuron[] outputLayer;
+	ByteBuffer labels;
+	Neuron[][] layers;
 	
-	public Net(int numInputNodes, int numHiddenNodes, int numOutputNodes) throws IOException{		
+	public Net(int numInputNodes, int numHiddenNodes, int numOutputNodes) {		
 		
-		//initialize input layer
-		Path path = Paths.get("train-images.idx3-ubyte");	
-		FileChannel file = FileChannel.open(path);			//open file
-		buffer = file.map(MapMode.READ_ONLY, 16, 60000);		//skip 16 byte header
-		inputLayer = new byte[28*28];						//initialize byte array
+		try {
+			//initialize input layer
+			Path imgPath = Paths.get("train-images.idx3-ubyte");	
+			FileChannel imgFile = null;
+			imgFile = FileChannel.open(imgPath);			//open file
+			buffer = imgFile.map(MapMode.READ_ONLY, 16, 60000*28*28);		//skip 16 byte header
+			inputLayer = new byte[28*28];						//initialize byte array
+			if(imgFile != null) {
+				imgFile.close(); 			//close file
+			}
+		} catch(Exception e) {
+			System.out.println(e.toString());
+		}
+		
 		
 		//initialize hidden layer
 		hiddenLayer = new Neuron[numHiddenNodes];
@@ -32,11 +44,31 @@ public class Net {
 			outputLayer[i] = new Neuron(numHiddenNodes);
 			outputLayer[i].layer = NetLayer.OUTPUTLAYER;
 		}
+		
+		try {
+			//get correct labels for each input image
+			labels = ByteBuffer.allocate(60000);	//sets the number of bytes to be held in the buffer
+			Path labelPath = Paths.get("train-labels.idx1-ubyte");	
+			FileChannel labelFile = null;
+			labelFile = FileChannel.open(labelPath);			//open file
+			labelFile.read(labels, 8);		//attempts to read as many bytes into labels ByteBuffer within its limit
+			if(labelFile != null) {
+				labelFile.close();
+			}
+		} catch(Exception e) {
+			System.out.println(e.toString());
+		}
+		
+		//initialize layers array
+		layers = new Neuron[2][];
+		layers[0] = hiddenLayer;
+		layers[1] = outputLayer;
 	}
 	
 	//method to read from the MappedByteBuffer of the images file to the inputLayer
 	public void loadInputLayer(int input) {
-		buffer.get(inputLayer, input*28*28, 28*28);
+		buffer.position(input*28*28);	//set next read position in buffer
+		buffer.get(inputLayer);		//reads as many bytes as can fit in inputLayer array
 	}
 	
 	//taking these inner products could probably be done more efficiently
@@ -68,5 +100,117 @@ public class Net {
 		for(Neuron node : outputLayer) {
 			calcNodeOutput(node);
 		}
+	}
+	
+	//C(w,b) = Sum_x ||y(x)-a||^2/(2n)
+	public double miniBatchCost(int miniBatch, int miniBatchSize, int[] shuffledList) {
+		double cost = 0.0;
+		double workingVector[] = new double[outputLayer.length];	//vector for y(x) - a
+		
+		for(int input=miniBatch*miniBatchSize; input<(miniBatch+1)*miniBatchSize; input++) {	//for each training input, x
+			loadInputLayer(input);	//puts training image data in input layer
+			calcNetOutput();	//find output, a	(stored in output nodes)
+			double sumSquares = 0.0;	//to hold ||y(x)-a||^2 for each input image, x
+			
+			for(int i=0; i<workingVector.length; i++) {	
+				//y(x) - a
+				//y(x) has 1.0 for expected output index and 0.0 for all others
+				if(i == labels.get(input)) {
+					workingVector[i] = 1.0 - outputLayer[i].output;
+				}
+				else { workingVector[i] = 0.0 - outputLayer[i].output; }
+				
+				//sum squares of difference vector to get ||y(x)-a||^2
+				sumSquares = sumSquares + workingVector[i]*workingVector[i];
+			}
+			
+			cost = cost + sumSquares;	//Sum_x ||y(x)-a||^2
+		}
+		cost = cost/(2*miniBatchSize);	//divide by 2n
+		
+		return cost;
+	}
+	
+	//dC/dw_i = (C_{w_i + eps} - C_{w_i})/eps
+	public void SGD(int miniBatch, int miniBatchSize, int[] shuffledList, double learningRate) {
+		double cost = miniBatchCost(miniBatch, miniBatchSize, shuffledList);	//calculate cost function for current weights/biases and miniBatch
+		
+		//update each weight/bias
+		for(Neuron[] layer : layers) {
+			for(Neuron node : layer) {		//for each node in the net
+				//for the node's weights
+				for(int i=0; i<node.inWeights.length; i++) {
+					double eps = calcEps(node.inWeights[i]);
+					node.inWeights[i] = node.inWeights[i] + eps;	//change to w_i + eps
+					double newCost = miniBatchCost(miniBatch, miniBatchSize, shuffledList);		//C_{w_i + eps}
+					double partialW = (newCost - cost)/eps;		//dC/dw_i = (C_{w_i + eps} - C_{w_i})/eps
+					node.inWeights[i] = node.inWeights[i] - eps - learningRate*partialW;	//w_i' = w_i - learningRate*dC/dw_i 
+				}
+				//for the node's bias
+				double eps = calcEps(node.bias);
+				node.bias = node.bias + eps;
+				double newCost = miniBatchCost(miniBatch, miniBatchSize, shuffledList);		//C_{w_i + eps}
+				double partialB = (newCost - cost)/eps;		//dC/dw_i = (C_{w_i + eps} - C_{w_i})/eps
+				node.bias = node.bias - eps - learningRate*partialB;	//b_i' = b_i - learningRate*dC/db_i
+			}
+		}
+	} 
+	
+	//EPSILON = x0*sqrt(Math.pow(2, -53))
+	//ensures round-off and truncation errors are of the same order
+	public static double calcEps(double x0) {
+		double eps;
+		double sqrtUlp = Math.sqrt(Math.pow(2, -53));	//the square root of the precision of the double type
+		eps = x0*sqrtUlp;
+		
+		if(eps != 0.0) {
+			return eps;
+		}
+		else {
+			return 0.01*sqrtUlp;	//arbitrary
+		}
+	}
+	
+	public double getErrorRate() throws IOException {		
+		//fill buffer with test set data
+		int numTestImages = 10000;
+		Path imgPath = Paths.get("t10k-images.idx3-ubyte");	
+		FileChannel imgFile = null;
+		imgFile = FileChannel.open(imgPath);			//open file
+		buffer = imgFile.map(MapMode.READ_ONLY, 16, numTestImages*28*28);		//skip 16 byte header
+		if(imgFile != null) {
+			imgFile.close(); 			//close file
+		}
+		
+		//get correct labels for each test image
+		labels.limit(10000);		//sets the number of bytes to be held in the buffer
+		Path labelPath = Paths.get("t10k-labels.idx1-ubyte");	
+		FileChannel labelFile = null;
+		labelFile = FileChannel.open(labelPath);			//open file
+		labelFile.read(labels, 8);		//attempts to read as many bytes into the labels ByteBuffer within its limit
+		if(labelFile != null) {
+			labelFile.close();
+		}
+		
+		int numWrong = 0;	//the number of test images the net classifies incorrectly
+		
+		for(int i=0; i<numTestImages; i++) {	//for each test image
+			loadInputLayer(i);	//load test image into input layer
+			calcNetOutput();	//get the output for this test image
+			int output = 0;		//the index of the output node with highest activation energy
+			double highest = 0.0; 	//said highest activation energy
+			for(int j=0; j<outputLayer.length; j++) {	//for each output node
+				if(outputLayer[j].output > highest) {	//find the one with highest activation energy
+					highest = outputLayer[j].output;
+					output = j;
+				}
+			}
+			//check if it matches the label
+			if(output != labels.get(i)) {
+				numWrong++;
+			}
+		}
+		
+		return ((double)numWrong)/(double)numTestImages;
 	}
 }
